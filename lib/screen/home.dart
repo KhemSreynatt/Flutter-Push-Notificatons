4e1ffd06242cd895cec60d210abda0d04f1e49e8
ramonoem <ramon.oem@chokchey.com.kh>
2023-07-18 09:36:42 +0700

    push notificaton

---
+++ lib/screen/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PushNotification extends StatelessWidget {
+  const PushNotification({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Push Notificaton"),
+      ),
+      body: ElevatedButton(
+        onPressed: () {},
+        child: const Text("Push Notificaton"),
+      ),
+    );
+  }
+}
